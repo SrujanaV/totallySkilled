668bce67a4a28472678dc5fc438e64dc06a9b830
snehalthakare <snehal.thakare@wohlig.com>
2017-08-09 19:22:54 +0530

    title of module detail page changed

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -4,4 +4,15 @@ myApp.controller('headerCtrl', function ($scope, TemplateService) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+
+    $scope.openLogin = function () {
+        $scope.loginModal = $uibModal.open({
+            animation: true,
+            templateUrl: 'views/modal/login.html',
+            scope: $scope,
+            windowClass: 'loginModalSize',
+            controller: 'loginModalCtrl'
+            // windowClass: 'modal-content-radi0'
+        });
+    }
 });
