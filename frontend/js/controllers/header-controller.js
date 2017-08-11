840aac1fa76f9be11a3011faff94756c75f12ae7
snehalthakare <snehal.thakare@wohlig.com>
2017-08-11 10:54:23 +0530

    login and sign up module

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,4 +1,4 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, $uibModal, TemplateService) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
@@ -6,13 +6,15 @@ myApp.controller('headerCtrl', function ($scope, TemplateService) {
     $.fancybox.close(true);
 
     $scope.openLogin = function () {
+        console.log("Hello");
         $scope.loginModal = $uibModal.open({
             animation: true,
             templateUrl: 'views/modal/login.html',
             scope: $scope,
-            windowClass: 'loginModalSize',
-            controller: 'loginModalCtrl'
-            // windowClass: 'modal-content-radi0'
+            windowClass: 'loginModalSize'
         });
     }
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 });
